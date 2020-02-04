c5422a27bc94a505e49a64af7020564eeb5590e9
TheKillerPepsi <44766164+TheKillerPepsi@users.noreply.github.com>
2020-02-04 23:00:39 +0100

    Add files via upload

---
+++ secondskript.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#https://null-byte.wonderhowto.com/how-to/hack-like-pro-python-scripting-for-aspiring-hacker-part-2-0159319/
+NullByteStringVariable = "Null Byte in wonderhowto.com is the Best Place on the Web to learn hacking!";
+NullByteIntegerVariable = 12
+NullByteFloatingPointVariable = 3.1415
+NullByteList = [1,2,3,4,5,6]
+NullByteDictionary = {'name': 'OccupytheWeb', 'value':12}
+print NullByteStringVariable
+print NullByteIntegerVariable
+print NullByteFloatingPointVariable
+print NullByteList[3]
